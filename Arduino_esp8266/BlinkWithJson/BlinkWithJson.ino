c0782f2feed27f2c870b041b102b423b0c4605b6
IhorHome <petrovytch@gmail.com>
2020-02-11 00:18:50 +0200

    add serializeJson for MQTT

--- Arduino_esp8266/BlinkWithJson/BlinkWithJson.ino
+++ Arduino_esp8266/BlinkWithJson/BlinkWithJson.ino
@@ -2,6 +2,9 @@
 
 #include "soilMoistureWithDeepSleep.h"
 
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
+
 // Configure web server
 ESP8266WebServer server(80);
 
@@ -15,6 +18,12 @@ char wifiSsid[32];
 char wifiPass[16];
 char mqttServer[16];
 
+// MC description
+char mcDescription[32];
+
+// web host address 
+char webHost[32];
+
 // Full MQTT prefix (with chip serial ID)
 char mqttPrefixNameS[32];
 uint8_t mqttPrefixNameLength;
@@ -22,18 +31,20 @@ uint8_t mqttPrefixNameLength;
 char mqttClientName[32];
 
 // Configure mode flag (if 1 - create AP with web server to configure wifi and mqtt server)
-uint8_t FlagConfigure;
+uint8_t FlagConfigure = 0;
 
-//WIFI 
 const char* host = "http://192.168.1.150";
 
+//WIFI 
+IPAddress localIP;
+/*
 IPAddress staticIP(192,168,1,71);  //static IP
 IPAddress gateway(192,168,1,150);
 IPAddress subnet(255,255,255,0);
 IPAddress DHCPServer(192,168,1,150);
-const char* ssid = "OpenWrt";
-const char* wifi_password = "charade23450";
 
+*/
+bool isFirstTime = true;
 
 template < typename T >
 void printlnDebug ( T param) {
@@ -48,6 +59,18 @@ void printDebug ( T param) {
       Serial.print(param);
 #endif   
 }
+
+char *IPAddress2String(IPAddress & ip) {
+    static char str_IP[16];
+    char * last = str_IP;
+    for (uint8_t i = 0; i < 4; i++) {
+        itoa(ip[i], last, 10);
+        last = last + strlen(last);
+        if (i == 3) *last = '\0'; else *last++ = '.';
+    }
+    return str_IP;
+}
+
 
 // Use chip ID to generate unique MQTT prefix
 void generateMqttName() {
@@ -67,6 +90,7 @@ void generateMqttName() {
 // Create web server in configure mode
 void createWebServer() {
   server.begin();
+//  server.handleClient();
   server.on("/", []() {
     String content;
     IPAddress ip = WiFi.softAPIP();
@@ -74,48 +98,81 @@ void createWebServer() {
     content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
     content += ipStr;
     content += "<p>";
-    content += "</p><form method='get' action='setting'><label>SSID:&nbsp;</label><input name='ssid' length=32 value='";
+    content += "</p><form method='get' action='setting'><label>SSID:_________&nbsp;</label><input name='ssid' length=32 value='";
     content += wifiSsid;
-    content += "'><BR><label>Password:&nbsp;</label><input name='pass' length=16>";
-    content += "<BR><label>MQTT&nbsp;broker&nbsp;IP:&nbsp;</label><input name='mqttip' length=15><P><input type='submit'></form>";
-    content += "</html>";
+    content += "'><BR><label>Password:______&nbsp;</label><input name='pass' length=16  value='";
+    content += wifiPass;
+    content += "'><BR><label>Description:____&nbsp;</label><input name='desc' length=32 value='";
+    content += mcDescription;
+    content += "'><BR><label>WEB Host Addr:&nbsp;</label><input name='host' length=32 value='";
+    content += webHost;
+    content += "'><BR><label>MQTT&nbsp;broker&nbsp;IP:&nbsp;</label><input name='mqttip' length=15 value='";
+    content += mqttServer;
+    content += "'><P><input type='submit'></form></html>";
     server.send(200, "text/html", content);
     startConfigure = millis();
   });
   server.on("/setting", []() {
-    int statusCode;
+    uint8_t statusCode;
+    uint8_t offset = 1;
     String content;
     String qsid = server.arg("ssid");
     String qpass = server.arg("pass");
+    String mcdesc = server.arg("desc");
+    String whost = server.arg("host");  
     String mqttip = server.arg("mqttip");
-    if ((qsid.length() > 0) && (qpass.length() > 0) && (mqttip.length() > 0)) {
+    if ((qsid.length() > 0) && (qpass.length() > 0) && (whost.length() > 0) && (mqttip.length() > 0)) {
       printlnDebug("Writing to EEPROM");
       printlnDebug("writing eeprom ssid:");
       EEPROM.begin(512);
+      // clear EEPROM
+      for (int i = 0; i < 512; i++) 
+        EEPROM.write(i, ' ');
+
       // Write signature
       EEPROM.write(0, 0xA5);
       strncpy(wifiSsid, qsid.c_str(), sizeof(wifiSsid) - 1);
       wifiSsid[sizeof(wifiSsid) - 1] = '\0';
       strncpy(wifiPass, qpass.c_str(), sizeof(wifiPass) - 1);
       wifiPass[sizeof(wifiPass) - 1] = '\0';
+      strncpy(webHost, whost.c_str(), sizeof(webHost) - 1);
+      webHost[sizeof(webHost) - 1] = '\0';
       strncpy(mqttServer, mqttip.c_str(), sizeof(mqttServer) - 1);
       mqttServer[sizeof(mqttServer) - 1] = '\0';
+      strncpy(mcDescription, mcdesc.c_str(), sizeof(mcDescription) - 1);
+      mcDescription[sizeof(mcDescription) - 1] = '\0';
       for (int i = 0; i < sizeof(wifiSsid); i++) {
-        EEPROM.write(i + 1, wifiSsid[i]);
+        EEPROM.write(i + offset, wifiSsid[i]);
         printlnDebug("Wrote: ");
         printlnDebug(wifiSsid[i]);
       }
+      offset += sizeof(wifiSsid);
       printlnDebug("writing eeprom pass:");
       for (int i = 0; i < sizeof(wifiPass); i++) {
-        EEPROM.write(i + sizeof(wifiSsid) + 1, wifiPass[i]);
+        EEPROM.write(i + offset, wifiPass[i]);
         printDebug("Wrote: ");
         printlnDebug(wifiPass[i]);
       }
+      offset += sizeof(wifiPass);
+      printlnDebug("writing eeprom web host ip:");
+      for (int i = 0; i < sizeof(webHost); i++) {
+        EEPROM.write(i + offset, webHost[i]);
+        printDebug("Wrote: ");
+        printlnDebug(webHost[i]);
+      }
+      offset += sizeof(webHost);
       printlnDebug("writing eeprom mqtt broker ip:");
       for (int i = 0; i < sizeof(mqttServer); i++) {
-        EEPROM.write(i + sizeof(wifiSsid) + sizeof(wifiPass) + 1, mqttServer[i]);
+        EEPROM.write(i + offset, mqttServer[i]);
         printDebug("Wrote: ");
         printlnDebug(mqttServer[i]);
+      }
+      offset += sizeof(mqttServer);
+      printlnDebug("writing eeprom mc description:");
+      for (int i = 0; i < sizeof(mcDescription); i++) {
+        EEPROM.write(i + offset, mcDescription[i]);
+        printDebug("Wrote: ");
+        printlnDebug(mcDescription[i]);
       }
       EEPROM.commit();
       EEPROM.end();
@@ -130,86 +187,135 @@ void createWebServer() {
     else {
       content = "{\"Error\":\"404 not found\"}";
       statusCode = 404;
-      Serial.println("Sending 404");
+      printlnDebug("Sending 404");
       server.send(statusCode, "application/json", content);
     }
   });
 } // void createWebServer()
 
 
-void connectWiFi() {
-
-//  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+bool connectWiFi() {
+  uint8_t iCount = 0;
+// We start by connecting to a WiFi network
+  printDebug("\nConnecting to ");
+  printlnDebug(wifiSsid);
 
 //  WiFi.mode(WIFI_AP_STA);
-  WiFi.config(staticIP, gateway, subnet, DHCPServer);
-  WiFi.begin(ssid, wifi_password);
+//  WiFi.config(staticIP, gateway, subnet, DHCPServer);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifiSsid, wifiPass);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    iCount++;
+    if ( iCount > WIFI_CONNECT_ATTEMPTS) 
+      return false;
   }
   // Debugging - Output the IP Address of the ESP8266
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  localIP = WiFi.localIP();
+  printlnDebug("\nWiFi connected");
+  printDebug("IP address: ");
+  printlnDebug(localIP);
+  return true;
 }
 
-void initMC() {
+bool connectMQTT() {
+  uint8_t iCount = 0;
+  mqttClient.setServer(mqttServer, 1883);
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    printlnDebug("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect(mqttClientName, MQTT_USER_NAME, MQTT_PASSWORD)) {
+      break;
+    } 
+    else {
+      iCount++;
+      if (iCount > MQTT_CONNECT_ATTEMPTS) {
+        printDebug("failed, rc=");
+        printlnDebug(mqttClient.state());
+        return false;
+      }
+      // Wait 5 seconds before retrying
+      delay(500);
+    }
+  }
+  printlnDebug("connected");
+  // Once connected, publish an announcement...
+  if (isFirstTime){
+    isFirstTime = false;        
+    int lenTopic = strlen(MQTT_PNP_PREFIX) + strlen(mqttClientName) + 2;
+    char topic[lenTopic];
+    snprintf(topic, lenTopic, "%s/%s", MQTT_PNP_PREFIX, mqttClientName);
+    StaticJsonDocument<256> doc;
+    doc["name"] = mqttClientName;
+    doc["ip"] = IPAddress2String(localIP);
+    serializeJson(doc, Serial);
+    char json_string[256];
+    serializeJson(doc, json_string);
+    printDebug("json_string: ");
+    printlnDebug(json_string);
+    mqttClient.publish(topic, json_string);
+    printlnDebug(topic);
+    printDebug(mqttClientName);
+    printlnDebug(" switched_on");
+  }
+  return true;  
+}
+
+
+bool initMC() {
   HTTPClient http;
-  Serial.printf("\n[Connecting to %s ... \n", host); //  Connecting
-  http.begin(host + String("/sendResponceToMC.php?name=MC1"));   
+  printDebug("\n[Connecting to "); //  Connecting
+  printlnDebug(webHost); 
+  http.begin(webHost + String("/sendResponceToMC.php?name=MC1"));   
 //    http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();
     //Check the returning code                                                                  
     if (httpCode == HTTP_CODE_OK) {
- 
-      // TODO: Parsing
+
      // DynamicJsonDocument doc(1024);
       StaticJsonDocument<1024> doc;
       auto error = deserializeJson(doc, http.getString());
       if (error) {
-         Serial.print(F("deserializeJson() failed with code "));
-         Serial.println(error.c_str());
-         return;
+         printDebug("deserializeJson() failed with code ");
+         printlnDebug(error.c_str());
+         return false;
       }
       int id = doc["id"]; 
-      Serial.println(id); 
+      printlnDebug(id); 
       const char* name = doc["name"];
-      Serial.println(name);
+      printlnDebug(name);
       JsonObject root = doc.as<JsonObject>();
       for (JsonPair p : root) {
          const char* key = p.key().c_str();
-         Serial.println(key);
+         printlnDebug(key);
          JsonVariant value = p.value();
          if (value.is<JsonArray>()) {
-            Serial.println("value is array");
+            printlnDebug("value is array");
          }
-       //     ...
       }
 
     }
     else {
       printlnDebug("error to connect to http server");
+      return false;
     }
     http.end();   //Close connection
-
-}
+    return true;
+}  // initMC()
 
 void startWebServer() {
     FlagConfigure = 1;
 //    WiFi.mode(WIFI_STA);
 //    WiFi.disconnect();
-    printDebug("Create AP to configure wifi parameters, SSID=");
+    printDebug("Create AP to configure WIFI and MQTT parameters, SSID=");
     printlnDebug(mqttClientName);
+    printlnDebug("ip address to connect setup server 192.168.4.1");
     WiFi.mode(WIFI_AP);
     WiFi.softAP(mqttClientName, WIFI_CONFIGURE_PASS);
+    
     createWebServer();
 }
 
@@ -223,28 +329,41 @@ void setup() {
   printDebug("Signature in EEPROM: ");
   printlnDebug(EEPROM.read(0));
 // Check if we have stored configuration data
+  generateMqttName();
   if (EEPROM.read(0) != 0xA5) {
     // No parameters in EEPROM, start configuration mode
     printlnDebug("No parameters in EEPROM, start configuration mode");
+    startWebServer();
   }
   else {
     printlnDebug("EEPROM has been already configured");
-//    signatureConf = true;
+    uint8_t offset = 1;
     for (int i = 0; i < sizeof(wifiSsid); i++)
-      wifiSsid[i] = EEPROM.read(i + 1);
+      wifiSsid[i] = EEPROM.read(i + offset);
+    offset += sizeof(wifiSsid);
     for (int i = 0; i < sizeof(wifiPass); i++)
-      wifiPass[i] = EEPROM.read(i + sizeof(wifiSsid) + 1);
+      wifiPass[i] = EEPROM.read(i + offset);
+    offset += sizeof(wifiPass);
+    for (int i = 0; i < sizeof(webHost); i++)
+      webHost[i] = EEPROM.read(i + offset);
+    offset += sizeof(webHost);
     for (int i = 0; i < sizeof(mqttServer); i++)
-      mqttServer[i] = EEPROM.read(i + sizeof(wifiSsid) + sizeof(wifiPass) + 1);
+      mqttServer[i] = EEPROM.read(i + offset);
+    offset += sizeof(mqttServer);  
+    for (int i = 0; i < sizeof(mcDescription); i++)
+      mcDescription[i] = EEPROM.read(i + offset);
     printlnDebug("Found WIFI parameters in EEPROM: ");
-    printDebug(wifiSsid);
+    if (!connectWiFi() or !connectMQTT() or !initMC()) {
+       printlnDebug("\nconnection error, start configuration mode");
+       startWebServer();
+    }
+    
+/*    printDebug(wifiSsid);
     printDebug(" ");
     printDebug(wifiPass);
     printDebug(" ");
-    printlnDebug(mqttServer);
+    printlnDebug(mqttServer);*/
   }
-  generateMqttName();
-  startWebServer();
     //signatureConf = false;
     //setupWifi(true);
   
@@ -257,9 +376,10 @@ void loop() {
                                     // but actually the LED is on; this is because 
                                     // it is active low on the ESP-01)
   delay(3000);                      // Wait for a second
-//  printlnDebug("LOW");
+  printlnDebug("LOW");
   digitalWrite(LED, HIGH);  // Turn the LED off by making the voltage HIGH
   delay(3000);                      // Wait for two seconds (to demonstrate the active low LED)
-//  printlnDebug("HIGH");
-  server.handleClient();
+  printlnDebug("HIGH");
+  if (FlagConfigure)
+    server.handleClient();
 }
